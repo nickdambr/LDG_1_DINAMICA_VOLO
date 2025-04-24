d7df09ba5b7c6a6b9caf25dea7cc19514fbe9442
Elisa2805 <jacopuccielisa@gmail.com>
2025-04-24 15:44:15 +0200

    update_x0_u0

--- airlib/Ldg_1_gruppo_6_2024_2025.m
+++ airlib/Ldg_1_gruppo_6_2024_2025.m
@@ -43,6 +43,9 @@ rho = 1.225*exp(-10^-4*H);
 
 [X0_air3m, U0_air3m] = air3m('airtrim',V,H,G);
 
+% X0_air3m = [95 0.0013 0 0 0 0 0 0.0013 0 0 0 270]
+% U0_air3m = [0.380596566005482 0 0 0 0 0 0.0238081811020551 0 0 0]
+
 %Impostazioni delle nuove variabili di spinta e angolo di equilibratore
 
 Fx0_air3m = U0_air3m(1)*10000;
