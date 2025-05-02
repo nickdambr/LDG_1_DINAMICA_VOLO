843fc28cf753838a402bf9e304bd1905840c7691
Francesco <francescodaniele03@gmail.com>
2025-05-02 16:57:28 +0200

    Update Ldg_1_gruppo_6_2024_2025_2.m

--- Fla_Nicco/Ldg_1_gruppo_6_2024_2025_2.m
+++ Fla_Nicco/Ldg_1_gruppo_6_2024_2025_2.m
@@ -111,7 +111,7 @@ switch grafico
         yline(0, '--k', 'Im = 0');  % Asse reale (orizzontale)
 end
 title('Diagramma zero-poli dinamica longitudinale')
-text(real(diag(EIGlong)), imag(diag(EIGlong)), {'1','2','3','4','5'}, 'VerticalAlignment','bottom','HorizontalAlignment','right');
+%text(real(diag(EIGlong)), imag(diag(EIGlong)), {'1','2','3','4','5'}, 'VerticalAlignment','bottom','HorizontalAlignment','right');
 
 % Tracciamento del Diagramma di Argand
 
